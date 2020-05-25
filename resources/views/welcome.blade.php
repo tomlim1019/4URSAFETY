3cd76f43af36ce5e75d6fb2ef102026d3e034028
tom <kienwah.lim@victoriahome.co>
2020-05-25 16:11:12 +0800

    reset

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,18 +81,13 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    4URSAFETY :D
+                    WELCOME TO 4URSAFETY
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="/login">LOGIN</a>
+                    <a href="/signup">SIGN UP</a>
+                    <a href="/product">PRODUCT</a>
                 </div>
             </div>
         </div>
