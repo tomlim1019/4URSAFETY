0559490742f6e87029d4543577cf24b305297f7c
tom lim <tomzaii971019@gmail.com>
2020-06-09 22:35:02 +0800

    testing

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,8 +12,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
-            // CategorySeeder::class,
-            // ProductSeeder::class,
+            CategorySeeder::class,
+            ProductSeeder::class,
             StaffSeeder::class,
         ]);
     }
