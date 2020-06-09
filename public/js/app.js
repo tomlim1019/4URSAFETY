4204887a1309c474ebddebfa895c34777deac371
xinruu <XxLimXinRuxX@gmail.com>
2020-06-09 23:23:02 +0800

    hi

--- public/js/app.js
+++ public/js/app.js
@@ -49839,8 +49839,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\tomza\Desktop\4URSAFETY\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\tomza\Desktop\4URSAFETY\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\Users\user\Desktop\4URSAFETY\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\user\Desktop\4URSAFETY\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
