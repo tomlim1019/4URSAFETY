cd5659d02b07c8531ff04ae1a0d9571e82b5322e
tom lim <tomzaii971019@gmail.com>
2020-06-20 22:04:53 +0800

    latest

--- database/migrations/2020_05_28_135610_create_products_table.php
+++ database/migrations/2020_05_28_135610_create_products_table.php
@@ -16,7 +16,7 @@ class CreateProductsTable extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('title');
-            $table->string('description');
+            $table->text('description');
             $table->decimal('price')->nullable();
             $table->enum('status', ['Active', 'Inactive'])->default('Active');
             $table->integer('tenure');
